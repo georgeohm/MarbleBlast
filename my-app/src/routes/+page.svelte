adbfaf90fca24b737fd082e47df53830145b506c
George Ohm <george.ohm.dot@gmail.com>
2023-08-12 14:56:33 -0500

    Box showing up

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import * as BABYLON from 'babylonjs'
+    import { onMount } from 'svelte'
+
+    let num = 1;
+    /**@type BABYLON.Engine*/ let engine;
+    /**@type HTMLCanvasElement*/ let canvas;
+    onMount(() => {
+            engine = new BABYLON.Engine(canvas, true)
+            const scene = new BABYLON.Scene(engine)
+            const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene)
+            camera.attachControl(canvas, true)
+
+            const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
+
+            const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene)
+
+            engine.runRenderLoop(() => scene.render())
+
+        return () => engine.stopRenderLoop()
+    })
+</script>
+<svelte:window on:resize={() => engine.resize()} />
+<h1>Welcome to SvelteKit {num}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<canvas bind:this={canvas}/>
