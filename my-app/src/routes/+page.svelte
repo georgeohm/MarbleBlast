61179680817fbd4a611410b41c3779af9d999b28
George Ohm <george.ohm.dot@gmail.com>
2023-08-12 15:01:32 -0500

    Made the canvas size fill screen

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -20,7 +20,14 @@
         return () => engine.stopRenderLoop()
     })
 </script>
+
 <svelte:window on:resize={() => engine.resize()} />
-<h1>Welcome to SvelteKit {num}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <canvas bind:this={canvas}/>
+
+<style>
+    canvas {
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
